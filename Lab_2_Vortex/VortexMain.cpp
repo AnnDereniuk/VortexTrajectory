3ff9462c460562aa5b6e47bbde5dce743827c225
Anna Dereniuk <danncem@gmail.com>
2019-03-12 23:32:05 +0200

    still not ready (not correct either)

--- Lab_2_Vortex/VortexMain.cpp
+++ Lab_2_Vortex/VortexMain.cpp
@@ -2,8 +2,8 @@
 #include<math.h>
 #define pi 3.14159265359
 #define e 2.71828182846
-#define y_0 -0.3
-#define x_0 -0.1
+#define y_0 3
+#define x_0 1
 #define intensity 1
 #define steptime 0.1
 
@@ -32,16 +32,33 @@ void display(void) {
 	double arrY[n];
 	arrX[0] = x_0;
 	arrY[0] = y_0;
-	glVertex2d(-1, -1); //õ
-	glVertex2d(1, -1);
-	glVertex2d(-1, 1); //ó
-	glVertex2d(-1, -1);
+	/*glColor3f(0.0, 0.0, 0.0);
+	glBegin(GL_LINES);
 
-	for (int i = 1; i <= n; i++) {
+	glVertex2d(-10, -10); //õ
+	glVertex2d(10, -10);
+
+	glVertex2d(-10, 10); //ó
+	glVertex2d(-10, -10);
+
+	/*for (int i = 1; i <= n; i++) {
 		arrX[i] = nextX(arrX[i - 1], arrY[i - 1]);
 		arrY[i] = nextY(arrX[i - 1], arrY[i - 1]);
 
 	}
+	glEnd();*/
+	glLineWidth(2);
+	glColor3f(1, 0, 0); 
+	glBegin(GL_LINE_STRIP);
+	glVertex2d(arrX[0], arrY[0]);
+	for (int i = 1; i < n; i++) {
+		arrX[i] = nextX(arrX[i - 1], arrY[i - 1]);
+		arrY[i] = nextY(arrX[i - 1], arrY[i - 1]);
+		glVertex2d(arrX[i], arrY[i]);
+	}
+	glEnd();
+	glFinish();
+
 }
 
 void init(void) {
